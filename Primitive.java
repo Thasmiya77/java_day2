9bd31309a66ba1438d19398071aa20cf26a57470
Thasmiya <galithasmiya@gmail.com>
2026-02-07 13:29:33 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Primitive {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sub1=sc.nextInt();
+        int sub2=sc.nextInt();
+        int sub3=sc.nextInt();
+        int total=sub1+sub2+sub3;
+        int avg=total/3;
+        System.out.println("total marks:"+total);
+        System.out.println("average:"+avg);
+
+    }
+    
+}
+
+    
+
