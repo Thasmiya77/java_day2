7911aabebd40b130d07ffae1e9442b4d95ef8570
Thasmiya <galithasmiya@gmail.com>
2026-02-07 14:39:07 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,26 @@
+public class Operators {
+    public static void main(String args[])  {
+        int a=31;
+        int b=65;
+        System.out.println("Sum:"+(a+b));
+        if(a>0)
+        {
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+        if(a>0&&a%2==0){
+            System.out.println("the num is positive and even");
+    }
+        else{
+            System.out.println("not a positive  and even");
+
+        }
+        int c=10;
+        System.out.println("c="+c);
+        System.out.println(++a);
+}
+
+    
+}
