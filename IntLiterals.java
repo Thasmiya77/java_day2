ca15c6a7b577907e0e0caeb5d53359addce5c94a
Thasmiya <galithasmiya@gmail.com>
2026-02-07 13:29:52 +0530

    Add files via upload

---
+++ IntLiterals.java
@@ -0,0 +1,14 @@
+class IntLiterals{
+    public static void main(String args[]) {
+        int a=55;
+        int b=053;
+        int c=0x31B;
+        int d=0b10;
+        char e='9';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
