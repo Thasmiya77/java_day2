cdcc7ba886b042eb7791276b01b1638af95e126a
Thasmiya <galithasmiya@gmail.com>
2026-02-07 14:59:08 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,14 @@
+public class Relational {
+    public static void main(String args[]) {
+       
+       float a=5.63f;
+       float b=7.3f;
+       System.out.println(a==b);
+       System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+    }
+    
+}
