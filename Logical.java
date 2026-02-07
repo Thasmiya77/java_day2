9f03608a8459f1c4a8a66ca438e2cea64af1a2e6
Thasmiya <galithasmiya@gmail.com>
2026-02-07 14:58:27 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,17 @@
+public class Logical {
+    public static void main(String args[])
+    {
+        boolean val=true;
+        int marks=97;
+        if(marks>=90&&marks<=100)
+        {
+            System.out.println("Grade:A");
+         }
+        else if(marks>=35||marks<=100)
+    { 
+       System.out.println("pass");
+    }   
+    System.out.println(!val); 
+}
+    
+}
