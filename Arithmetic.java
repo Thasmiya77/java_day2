85c3e5c129f167b5279183ed4f93f6026c4eac72
Thasmiya <galithasmiya@gmail.com>
2026-02-07 14:57:40 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic {
+    public static void main(String args[]) {
+    int a=5;
+    int b=15;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+    }
+}
